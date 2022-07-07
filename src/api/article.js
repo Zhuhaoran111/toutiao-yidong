de01f962c710c104392ec2acecadf59466c1c9b1
zhuhaoran <zhuhaoran@ambition.com>
2022-07-07 15:37:26 +0800

    频道编辑

--- src/api/article.js
+++ src/api/article.js
@@ -1,12 +1,17 @@
-// 文章请求模块
+/**
+ * 文章请求模块
+ */
+ import request from '@/utils/request'
 
-import request from '@/utils/request'
-
-export const getArticles=(params)=>{
-    return request({
-        method:'GET',
-        url:'/v1_0/articles',
-        //这里直接传一个对象
-        params
-    })
-}
+ /**
+  * 获取文章列表
+  */
+ export const getArticles = params => {
+   return request({
+     method: 'GET',
+     url: '/v1_0/articles',
+     // params 选项用来配置 Query 参数
+     params
+   })
+ }
+ 
